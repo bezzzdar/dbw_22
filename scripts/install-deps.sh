a587e51b6c6e37f14239073fda0158c6a9498b95
Konstantin Nazarov <kostr2010@gmail.com>
2020-11-01 19:36:27 +0300

    script fixes

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -23,15 +23,18 @@ done
 
 if [[ ${pac_man} == "pacman" ]]
 then
+	echo -e "\033[1;32m OS identified as arch linux, using pacman to install...\033[0m"
 	sudo pacman -Syu cmake make gcc mysql binutils openssl boost zlib curl
 elif [[ ${pac_man} == "apt-get" ]]
 then
+	echo -e "\033[1;32m OS identified as debian-like, using apt-get to install...\033[0m"
 	sudo apt-get install cmake make gcc mysql libssl-dev libboost-system-dev zlib1g-dev libcurl4-openssl-dev
 elif [[ ${pac_man} == "dnf" ]]
 then 
+	echo -e "\033[1;32m OS identified as fedora or another red hat product, using dnf to install...\033[0m"
 	sudo dnf install cmake make gcc mysql
 else 
-	echo -e "\033[1;31m current version of linux distro is not supported. modify /scripts/install-deps.sh arrcordingly \033[0m"
+	echo -e "\033[1;31m current version of linux distro is not supported. modify /scripts/install-deps.sh arrcordingly!\033[0m"
 fi
 
 ####################
